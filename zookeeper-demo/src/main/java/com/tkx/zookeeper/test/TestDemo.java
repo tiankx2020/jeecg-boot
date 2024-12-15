538f1a1838112eb75fe3e70e19b487655fca8034
tiankx2020 <2633385761@qq.com>
2024-12-15 21:44:36 +0800

    添加RedisDemo

--- zookeeper-demo/src/main/java/com/tkx/zookeeper/test/TestDemo.java
+++ zookeeper-demo/src/main/java/com/tkx/zookeeper/test/TestDemo.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.*;
 
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @Author tkx
